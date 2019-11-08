54b55ed955c9bc25c3f00d12f6f86d8bad2ab0b7
Greg King <king.greg@gmail.com>
2019-11-08 13:16:04 -0500

    Initial

---
+++ src/main/java/king/greg/aoc2019/App.java
@@ -0,0 +1,13 @@
+package king.greg.aoc2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
