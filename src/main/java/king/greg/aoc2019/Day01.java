785b8ce4d57d90cf3a84983da6eee5876bb13e82
Greg King <king.greg@gmail.com>
2019-12-01 00:25:32 -0500

    Day 01

--- src/main/java/king/greg/aoc2019/Day01.java
+++ src/main/java/king/greg/aoc2019/Day01.java
@@ -1,5 +1,57 @@
 package king.greg.aoc2019;
 
-public class Day01 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Day01 {
+	
+	long fuel;
+	long fuel2;
+
+	public Day01(FileReader fileReader) {
+        try {
+            final BufferedReader buf = new BufferedReader(fileReader);
+            fuel = 0;
+
+            while(true) {
+                final String lineJustFetched = buf.readLine();
+                if(null == lineJustFetched) {
+                    break;
+                } else {
+                    long mass = Long.valueOf(lineJustFetched);
+                    long requiredFuel = Math.floorDiv(mass, 3) -2;
+                    fuel += requiredFuel;
+                    fuel2 += calcTotalFuel(mass);
+                }
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException();
+        }
+	}
+	
+	long calcTotalFuel(long mass) {
+		long newFuel = 0;
+		long currentMass = mass;
+
+		while (true) {
+			currentMass = Math.floorDiv(currentMass, 3) - 2;
+			if (currentMass > 0) {
+				newFuel += currentMass;
+			} else {
+				break;
+			}
+		}
+		
+		return newFuel;
+	}
+	
+	public long getFuel() {
+		return fuel;
+	}
+
+	public long getFuel2() {
+		return fuel2;
+	}
+	
 }
--- src/test/java/king/greg/aoc2019/Day01Test.java
+++ src/test/java/king/greg/aoc2019/Day01Test.java
@@ -1,15 +1,26 @@
 package king.greg.aoc2019;
 
 import org.assertj.core.api.Assertions;
-import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import org.junit.Test;
 
 public class Day01Test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSolution1() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day01/input.txt").getPath());
+        final Day01 day01 = new Day01(fileReader);
+        Assertions.assertThat(day01.getFuel()).isEqualTo(3412207);
+	}
+	
+	@Test
+	public void testSolution2() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day01/input.txt").getPath());
+        final Day01 day01 = new Day01(fileReader);
+        Assertions.assertThat(day01.getFuel2()).isEqualTo(5115436);
 	}
 
 }
