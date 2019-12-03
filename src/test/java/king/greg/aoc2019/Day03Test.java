e3db9045bffb60f382bae3bcda202a9425b54b03
Greg King <greg.king@gfs.com>
2019-12-03 13:12:49 -0500

    Fixing some crap

--- src/test/java/king/greg/aoc2019/Day03Test.java
+++ src/test/java/king/greg/aoc2019/Day03Test.java
@@ -9,14 +9,14 @@ import org.junit.Test;
 public class Day03Test {
 
 	@Test
-	public void testsolution1() throws FileNotFoundException {
+	public void testSolution1() throws FileNotFoundException {
         final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day03/input.txt").getPath());
         final Day03 day03 = new Day03(fileReader);
         Assertions.assertThat(day03.closestIntersection()).isEqualTo(5319);
 	}
 	
 	@Test
-	public void testsolution2() throws FileNotFoundException {
+	public void testSolution2() throws FileNotFoundException {
         final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day03/input.txt").getPath());
         final Day03 day03 = new Day03(fileReader);
         Assertions.assertThat(day03.fastestIntersection()).isEqualTo(122514);
