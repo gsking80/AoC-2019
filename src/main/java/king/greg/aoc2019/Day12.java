8a3b99011641d09bc3d0d2b86062603e087279a2
Greg King <greg.king@gfs.com>
2019-12-12 15:35:45 -0500

    Fixing some crap

--- src/main/java/king/greg/aoc2019/Day12.java
+++ src/main/java/king/greg/aoc2019/Day12.java
@@ -1,102 +1,156 @@
 package king.greg.aoc2019;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.IntPredicate;
+import java.util.*;
 
 public class Day12 {
-	
-	final Set<Moon> moons = new HashSet<>();
-	
-	private void updateVelocities() {
-		for (final Moon moon: moons) {
-			for (final Moon otherMoon: moons) {
-				if (moon.equals(otherMoon)) {
+
+    final List<Moon> moons = new ArrayList<>();
+
+    private void updateVelocities() {
+        for (final Moon moon : moons) {
+            for (final Moon otherMoon : moons) {
+                if (moon.equals(otherMoon)) {
+                    continue;
+                }
+                moon.applyGravity(otherMoon);
+            }
+        }
+    }
+
+    private void updatePositions() {
+        for (final Moon moon : moons) {
+            moon.updatePosition();
+        }
+    }
+
+    private int calculateEnergy() {
+        int energy = 0;
+
+        for (final Moon moon : moons) {
+            energy += moon.getEnergy();
+        }
+
+        return energy;
+    }
+
+    public int simulate(final int steps) {
+        for (int i = 0; i < steps; i++) {
+            step();
+        }
+        printMoons();
+        return calculateEnergy();
+    }
+
+    private void printMoons() {
+        for(final Moon moon: moons) {
+            System.out.println(Arrays.toString(moon.locations) + " - " + Arrays.toString(moon.velocities));
+        }
+    }
+
+    private void step() {
+		updateVelocities();
+		updatePositions();
+	}
+
+    public long firstRepeat() {
+        final long[] periods = new long[3];
+        final List<Set<List<Integer>>> states = new ArrayList<>();
+        for (long i = 0; i < 3; i++) {
+        	states.add(new HashSet<>());
+		}
+		int steps = 0;
+        while(periods[0] == 0 || periods[1] == 0 || periods[2] == 0) {
+
+			for (int i = 0; i < 3; i++){
+				if (periods[i] != 0) {
 					continue;
 				}
-				moon.applyGravity(otherMoon);
+				List<Integer> state = new ArrayList<>();
+				for (final Moon moon: moons) {
+					state.add(moon.locations[i]);
+					state.add(moon.velocities[i]);
+				}
+				if (states.get(i).contains(state)) {
+                        periods[i] = steps;
+				} else {
+					states.get(i).add(state);
+				}
 			}
-		}
-	}
-	
-	private void updatePositions() {
-		for (final Moon moon: moons) {
-			moon.updatePosition();
-		}
-	}
-	
-	private int calculateEnergy() {
-		int energy = 0;
-		
-		for (final Moon moon: moons) {
-			energy += moon.getEnergy();
-		}
-		
-		return energy;
-	}
-	
-	public int simulate(final int steps) {
-		for (int i = 0; i < steps; i++) {
-			updateVelocities();
-			updatePositions();
-		}
-		return calculateEnergy();
-	}
 
-	public IntPredicate firstRepeat() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public void addMoon(final int x, final int y, final int z) {
-		moons.add(new Moon(x,y,z));
-	}
-	
-	public class Moon {
-
-		int xLoc;
-		int yLoc;
-		int zLoc;
-		int xVel;
-		int yVel;
-		int zVel;
-		
-		public Moon(final int x, final int y, final int z) {
-			xLoc = x;
-			yLoc = y;
-			zLoc = z;
+			step();
+			steps++;
 		}
 
-		public int getEnergy() {
-			return (Math.abs(xLoc) + Math.abs(yLoc) + Math.abs(zLoc)) * (Math.abs(xVel) + Math.abs(yVel) + Math.abs(zVel));
-		}
+        return lcm(periods);
+    }
 
-		public void updatePosition() {
-			xLoc += xVel;
-			yLoc += yVel;
-			zLoc += zVel;
-		}
+    private static long lcm(final long[] array) {
+        long lcm = 1;
+        for (long value : array) {
+            lcm = lcm(lcm, value);
+        }
+        return lcm;
+    }
 
-		public void applyGravity(final Moon otherMoon) {
-			if (xLoc < otherMoon.xLoc) {
-				xVel++;
-			} else if (xLoc > otherMoon.xLoc) {
-				xVel--;
+    private static long lcm(final long a, final long b) {
+        if (a == 0 || b == 0) {
+        	return 0;
+		}
+    	return ((a * b) / gcd(a, b));
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return 0;
+        } else if (a < b) {
+            return gcd(b, a);
+        } else {
+            while (b > 0) {
+                long tempA = a;
+                a = b;
+                b = tempA % b;
+            }
+            return a;
+        }
+    }
+
+    public void addMoon(final int x, final int y, final int z) {
+        moons.add(new Moon(x, y, z));
+    }
+
+    public class Moon {
+
+    	int[] locations = new int[3];
+    	int[] velocities = new int[3];
+
+        public Moon(final int x, final int y, final int z) {
+            locations[0] = x;
+            locations[1] = y;
+            locations[2] = z;
+        }
+
+        public int getEnergy() {
+        	final int potential = Arrays.stream(locations).map(a -> Math.abs(a)).sum();
+        	final int kinetic = Arrays.stream(velocities).map(a -> Math.abs(a)).sum();
+            return potential * kinetic;
+        }
+
+        public void updatePosition() {
+        	for (int i = 0; i < 3; i++) {
+				locations[i] += velocities[i];
 			}
-			
-			if (yLoc < otherMoon.yLoc) {
-				yVel++;
-			} else if (yLoc > otherMoon.yLoc) {
-				yVel--;
+        }
+
+        public void applyGravity(final Moon otherMoon) {
+        	for (int i = 0; i < 3; i++) {
+        		if (locations[i] < otherMoon.locations[i]) {
+        			velocities[i]++;
+				} else if (locations[i] > otherMoon.locations[i]) {
+        			velocities[i]--;
+				}
 			}
-			
-			if (zLoc < otherMoon.zLoc) {
-				zVel++;
-			} else if (zLoc > otherMoon.zLoc) {
-				zVel--;
-			}
-		}
-		
-	}
+        }
+
+    }
 
 }
--- src/test/java/king/greg/aoc2019/Day12Test.java
+++ src/test/java/king/greg/aoc2019/Day12Test.java
@@ -14,7 +14,29 @@ public class Day12Test {
 		day12.addMoon(0, 8, 8);
 		Assertions.assertThat(day12.simulate(1000)).isEqualTo(10198);
 	}
-	
+
+	@Test
+	public void test2a() {
+		final Day12 day12 = new Day12();
+		day12.addMoon(-1, 0, 2);
+		day12.addMoon(2, -10, -7);
+		day12.addMoon(4, -8, 8);
+		day12.addMoon(3, 5, -1);
+		Assertions.assertThat(day12.firstRepeat()).isEqualTo(2772);
+	}
+
+	@Test
+	public void test2b() {
+		final Day12 day12 = new Day12();
+		day12.addMoon(-8, -10, 0);
+		day12.addMoon(5, 5,10);
+		day12.addMoon(2, -7, 3);
+		day12.addMoon(9, -8, -3);
+		Assertions.assertThat(day12.firstRepeat()).isEqualTo(4686774924L);
+	}
+
+
+
 	@Test
 	public void testSolution2() {
 		final Day12 day12 = new Day12();
@@ -22,7 +44,7 @@ public class Day12Test {
 		day12.addMoon(18, -7, 13);
 		day12.addMoon(16, 3, 4);
 		day12.addMoon(0, 8, 8);
-		Assertions.assertThat(day12.firstRepeat()).isEqualTo(10198);
+		Assertions.assertThat(day12.firstRepeat()).isEqualTo(271442326847376L);
 	}
 
 }
