b279309f14e83fcb0a5b97a087a44d98e49dff7d
Greg King <greg.king@gfs.com>
2020-11-29 14:15:28 -0500

    2020 Initial Setup

--- src/test/java/king/greg/aoc2019/Day01Test.java
+++ src/test/java/king/greg/aoc2019/Day01Test.java
@@ -4,6 +4,12 @@ import org.assertj.core.api.Assertions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -22,5 +28,54 @@ public class Day01Test {
         final Day01 day01 = new Day01(fileReader);
         Assertions.assertThat(day01.getFuel2()).isEqualTo(5115436);
 	}
+
+	@Test
+    public void testStuff() {
+
+        Locale[] locales = NumberFormat.getAvailableLocales();
+        double myNumber = -1234.56;
+        NumberFormat form;
+        for (int j = 0; j < 4; ++j) {
+            System.out.println("FORMAT");
+            for (int i = 0; i < locales.length; ++i) {
+                if (locales[i].getCountry().length() == 0) {
+                    continue; // Skip language-only locales
+                }
+                System.out.print(locales[i].getDisplayName());
+                switch (j) {
+                    case 0:
+                        form = NumberFormat.getInstance(locales[i]); break;
+                    case 1:
+                        form = NumberFormat.getIntegerInstance(locales[i]); break;
+                    case 2:
+                        form = NumberFormat.getCurrencyInstance(locales[i]); break;
+                    default:
+                        form = NumberFormat.getPercentInstance(locales[i]); break;
+                }
+                if (form instanceof DecimalFormat) {
+                    System.out.print(": " + ((DecimalFormat) form).toPattern());
+                }
+                System.out.print(" -> " + form.format(myNumber));
+                try {
+                    System.out.println(" -> " + form.parse(form.format(myNumber)));
+                } catch (ParseException e) {}
+            }
+        }
+
+//        for (Locale locale : DecimalFormat.getAvailableLocales()) {
+//            DecimalFormat df;
+//            System.out.println(df.set);
+//        }
+
+//	    BigDecimal blah = new BigDecimal("123.45-");
+    }
+
+    @Test
+    public void testThis() {
+	    Date thisdate = new Date(123434455);
+	    System.out.println(thisdate);
+
+	    System.out.println(thisdate.toInstant());
+    }
 
 }
